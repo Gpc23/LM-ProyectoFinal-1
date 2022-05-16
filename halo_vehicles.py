48e62cdb2fd464631af2f680e89af773de67693e
Gpc23 <pcg.200204@gmail.com>
2022-05-16 12:01:45 +0200

    Añadido tercer programa python

---
+++ halo_vehicles.py
@@ -0,0 +1,19 @@
+import http.client, urllib.request, urllib.parse, urllib.error, base64
+
+headers = {
+    'Accept-Language': 'en',
+    'Ocp-Apim-Subscription-Key': 'key',
+}
+
+params = urllib.parse.urlencode({
+})
+
+try:
+    conn = http.client.HTTPSConnection('www.haloapi.com')
+    conn.request("GET", "/metadata/h5/metadata/vehicles?%s" % params, "{body}", headers)
+    response = conn.getresponse()
+    data = response.read()
+    print(data)
+    conn.close()
+except Exception as e:
+    print("[Errno {0}] {1}".format(e.errno, e.strerror))
